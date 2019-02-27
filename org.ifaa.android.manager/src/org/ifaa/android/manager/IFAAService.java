LineageOS/android_device_xiaomi_jason

jason: Access mlipay hal via a binder service
Change-Id: Ie52376b1f7bcd84d219fe73bceb4bdd6cc2b9980

IFAAService/src/org/ifaa/android/manager/IFAAService.java
@@ -1,0 +1,66 @@
+package org.ifaa.android.manager;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.HwBinder;
+import android.os.HwBlob;
+import android.os.HwParcel;
+import android.os.IBinder;
+import android.os.IHwBinder;
+import android.os.RemoteException;
+import android.util.Slog;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class IFAAService extends Service {
+    private static final String TAG = "IFAAService";
+
+    private final IBinder mIFAABinder = new IIFAAService.Stub() {
+        private static final String INTERFACE_DESCRIPTOR =
+                "vendor.xiaomi.hardware.mlipay@1.0::IMlipayService";
+        private static final String SERVICE_NAME =
+                "vendor.xiaomi.hardware.mlipay@1.0::IMlipayService";
+
+        private static final int CODE_PROCESS_CMD = 1;
+
+        private IHwBinder mService;
+
+        @Override
+        public byte[] processCmd_v2(byte[] param) {
+            HwParcel hwParcel = new HwParcel();
+            try {
+                if (mService == null) {
+                    mService = HwBinder.getService(SERVICE_NAME, "default");
+                }
+                if (mService != null) {
+                    HwParcel hwParcel2 = new HwParcel();
+                    hwParcel2.writeInterfaceToken(INTERFACE_DESCRIPTOR);
+                    ArrayList arrayList = new ArrayList(Arrays.asList(HwBlob.wrapArray(param)));
+                    hwParcel2.writeInt8Vector(arrayList);
+                    hwParcel2.writeInt32(arrayList.size());
+                    mService.transact(CODE_PROCESS_CMD, hwParcel2, hwParcel, 0);
+                    hwParcel.verifySuccess();
+                    hwParcel2.releaseTemporaryStorage();
+                    ArrayList readInt8Vector = hwParcel.readInt8Vector();
+                    int size = readInt8Vector.size();
+                    byte[] result = new byte[size];
+                    for (int i = 0; i < size; i++) {
+                        result[i] = ((Byte) readInt8Vector.get(i)).byteValue();
+                    }
+                    return result;
+                }
+            } catch (RemoteException e) {
+                Slog.e(TAG, "transact failed. " + e);
+            } finally {
+                hwParcel.release();
+            }
+            Slog.e(TAG, "processCmdV2, return null");
+            return null;
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mIFAABinder;
+    }
+}

rsyhan/android_device_xiaomi_platina

 * Untrusted apps are no longer allowed to directly access hwbinder on
   Pie, implement a system service as a middleware between mlipay hal
   and untrusted apps.
 * Xiaomi uses similar solution for MIUI on Pie.
Signed-off-by: RainSnowHan <2652609017@qq.com>

org.ifaa.android.manager/src/org/ifaa/android/manager/IFAAService.java
@@ -1,0 +1,67 @@
+package org.ifaa.android.manager;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.HwBinder;
+import android.os.HwBlob;
+import android.os.HwParcel;
+import android.os.IBinder;
+import android.os.IHwBinder;
+import android.os.RemoteException;
+import android.util.Slog;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class IFAAService extends Service {
+    private static final boolean DEBUG = false;
+    private static final String TAG = "IFAAService";
+
+    private final IBinder mIFAABinder = new IIFAAService.Stub() {
+        private static final String INTERFACE_DESCRIPTOR =
+                "vendor.xiaomi.hardware.mlipay@1.0::IMlipayService";
+        private static final String SERVICE_NAME =
+                "vendor.xiaomi.hardware.mlipay@1.0::IMlipayService";
+
+        private static final int CODE_PROCESS_CMD = 1;
+
+        private IHwBinder mService;
+
+        @Override
+        public byte[] processCmd_v2(byte[] param) {
+            HwParcel reply = new HwParcel();
+            try {
+                if (mService == null) {
+                    mService = HwBinder.getService(SERVICE_NAME, "default");
+                }
+                if (mService != null) {
+                    HwParcel data = new HwParcel();
+                    data.writeInterfaceToken(INTERFACE_DESCRIPTOR);
+                    ArrayList arrayList = new ArrayList(Arrays.asList(HwBlob.wrapArray(param)));
+                    data.writeInt8Vector(arrayList);
+                    data.writeInt32(arrayList.size());
+                    mService.transact(CODE_PROCESS_CMD, data, reply, 0);
+                    reply.verifySuccess();
+                    data.releaseTemporaryStorage();
+                    ArrayList readInt8Vector = reply.readInt8Vector();
+                    int size = readInt8Vector.size();
+                    byte[] result = new byte[size];
+                    for (int i = 0; i < size; i++) {
+                        result[i] = ((Byte) readInt8Vector.get(i)).byteValue();
+                    }
+                    return result;
+                }
+            } catch (RemoteException e) {
+                if (DEBUG) Slog.e(TAG, "transact failed. " + e);
+            } finally {
+                reply.release();
+            }
+            if (DEBUG) Slog.e(TAG, "processCmdV2, return null");
+            return null;
+        }
+    };
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mIFAABinder;
+    }
+}