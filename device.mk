081819932865c31ce68f976bc7b684e4a4ed95ac
RainSnowHan <2652609017@qq.com>
2019-02-28 08:36:43 +0800

    Revert "jason: Access mlipay hal via a binder service"
    This reverts commit ca58cc43f5ebd2bdf7a54c9ab69366816d71c2ab.
    
    Signed-off-by: RainSnowHan <2652609017@qq.com>

--- device.mk
+++ device.mk
@@ -93,7 +93,6 @@ PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/whitelistedapps.xml:$(TARGET_COPY_OU
 
 # Alipay
 PRODUCT_PACKAGES += \
-    IFAAService \
     org.ifaa.android.manager
 
 PRODUCT_BOOT_JARS += \
