2bb860f3ad8062fa39d931ded3cdd1f710e81589
RainSnowHan <2652609017@qq.com>
2019-03-08 16:37:16 +0800

    platina: Import XiaomiParts and KCal  * Import XiaomiParts and KCal from https://github.com/xiaomi-sdm660  * Add Lineage KeyHandler  * Removed some unused languages strings  * Adapt Camera HAL3 Enable for P
    Signed-off-by: RainSnowHan <2652609017@qq.com>

--- device.mk
+++ device.mk
@@ -475,6 +475,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# KeyHandler
+PRODUCT_PACKAGES += \
+    org.lineageos.keyhandler
+
 # XiaomiParts
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    XiaomiParts \
+    KCal
