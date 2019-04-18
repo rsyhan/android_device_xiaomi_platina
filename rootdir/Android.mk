32e38531fe93bd658ebfe1fd05b6feb50e0d01d0
RainSnowHan <2652609017@qq.com>
2019-04-18 23:01:49 +0800

    platina: Update rootdir  * Move two roodir inits into a common init  * Enable BFQ I/O scheduler once boot is complete  * Speed up boot speed  * Enable KCAL support  * BBR will be auto enabled by kernel
    Signed-off-by: RainSnowHan <2652609017@qq.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -20,18 +20,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.parts.rc
+LOCAL_MODULE       := init.rsyhan.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.parts.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.spectrum.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.spectrum.rc
+LOCAL_SRC_FILES    := etc/init.rsyhan.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
