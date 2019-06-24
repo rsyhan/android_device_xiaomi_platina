2c3f483114fef5ea88e8628efe22ab59a5a2d263
RainSnowHan <yuhan@rsyhan.me>
2019-06-24 20:39:58 +0800

    platina: Import Power HALs from @nysadev
    Signed-off-by: RainSnowHan <yuhan@rsyhan.me>

--- device.mk
+++ device.mk
@@ -337,7 +337,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service.sdm660-libperfmgr
+
+PRODUCT_COPY_FILES += \
+   $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
